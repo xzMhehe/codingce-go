38e0f96a36ec27f1205f29a27de366fd157176a8
Ma Xinze <2460798168@qq.com>
2021-02-26 20:33:46 +0800

    :fire: 基本运算

---
+++ src/gocode/operation.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// 算数运算符
+	fmt.Println("===算数运算符===")
+	fmt.Println("+:	相加")
+	fmt.Println("-:	相减")
+	fmt.Println("*:	相乘")
+	fmt.Println("/:	相除")
+	fmt.Println("%:	求余")
+
+	// 算数运算符
+	fmt.Println("===算数运算符===")
+	fmt.Println("+:	相加")
+	fmt.Println("-:	相减")
+	fmt.Println("*:	相乘")
+	fmt.Println("/:	相除")
+	fmt.Println("%:	求余")
+
+}
